4fbeeb503c5247508707aa11c28ce750c5c51343
Stephen Young <young.steveo@gmail.com>
2023-06-04 12:52:18 -0400

    Fix regression in test suite that removed coverage.

--- tests/Codex/Fixture/ParentClass.php
+++ tests/Codex/Fixture/ParentClass.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Arcanum\Test\Codex\Fixture;
 
-class ParentClass extends SimpleClass
+class ParentClass extends SimpleDependency
 {
     public SimpleDependency $dependency;
-    public function __construct(SimpleDependency $dependency)
+    public function __construct(parent $dependency)
     {
         $this->dependency = $dependency;
     }
