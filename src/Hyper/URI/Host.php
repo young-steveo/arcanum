9d58f34cbdf3e4223ff348c509230ba12f63b5ee
Stephen Young <young.steveo@gmail.com>
2023-06-23 17:40:26 -0400

    Document and simplify Host and Scheme

--- src/Hyper/URI/Host.php
+++ src/Hyper/URI/Host.php
@@ -6,23 +6,33 @@ namespace Arcanum\Hyper\URI;
 
 final class Host implements \Stringable
 {
-    public const DEFAULT_HTTP_HOST = 'localhost';
-
+    /**
+     * Host
+     */
     public function __construct(private string $value)
     {
         $this->value = strtolower($value);
     }
 
+    /**
+     * Create a localhost host.
+     */
     public static function localhost(): self
     {
-        return new self(self::DEFAULT_HTTP_HOST);
+        return new self('localhost');
     }
 
+    /**
+     * Host is a string.
+     */
     public function __toString(): string
     {
         return $this->value;
     }
 
+    /**
+     * Check if the host is empty.
+     */
     public function isEmpty(): bool
     {
         return $this->value === '';
--- src/Hyper/URI/Scheme.php
+++ src/Hyper/URI/Scheme.php
@@ -6,24 +6,33 @@ namespace Arcanum\Hyper\URI;
 
 final class Scheme implements \Stringable
 {
-    public const HTTP = 'http';
-    public const HTTPS = 'https';
-
+    /**
+     * Scheme
+     */
     public function __construct(private string $value)
     {
         $this->value = strtolower($value);
     }
 
+    /**
+     * Scheme as a string.
+     */
     public function __toString(): string
     {
         return $this->value;
     }
 
+    /**
+     * Check if the scheme is a web scheme.
+     */
     public function isWebScheme(): bool
     {
-        return $this->value === self::HTTP || $this->value === self::HTTPS;
+        return $this->value === 'http' || $this->value === 'https';
     }
 
+    /**
+     * Check if the scheme is empty.
+     */
     public function isEmpty(): bool
     {
         return $this->value === '';
