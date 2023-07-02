60ac61a8837462d4eaa753ca1bff143f92434526
Stephen Young <young.steveo@gmail.com>
2023-07-02 17:29:02 -0400

    Move container integration tests to their rightful home.

--- tests/Cabinet/IntegrationTest.php
+++ tests/Cabinet/IntegrationTest.php
@@ -6,6 +6,7 @@ namespace Arcanum\Test\Cabinet;
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\Attributes\UsesClass;
 use Arcanum\Cabinet\Container;
 use Arcanum\Test\Fixture;
@@ -235,5 +236,81 @@ final class IntegrationTest extends TestCase
         $this->assertTrue($container->has('custom'));
         $result = $container->get('custom');
         $this->assertInstanceOf(Fixture\SimpleService::class, $result);
+    }
+
+
+    #[CoversNothing]
+    public function testContainerService(): void
+    {
+        // Arrange
+        $container = new Container();
+        $container->service(Fixture\SimpleClass::class);
+
+        // Act
+        $result = $container->get(Fixture\SimpleClass::class);
+
+        // Assert
+        $this->assertInstanceOf(Fixture\SimpleClass::class, $result);
+        $this->assertSame($container->get(Fixture\SimpleClass::class), $result);
+    }
+
+    #[CoversNothing]
+    public function testResolveDependencies(): void
+    {
+        // Arrange
+        $container = new Container();
+        $container->service(Fixture\SimpleClass::class);
+        $container->service(Fixture\SimpleDependency::class);
+
+        // Act
+        $result = $container->get(Fixture\SimpleClass::class);
+
+        // Assert
+        $this->assertInstanceOf(Fixture\SimpleClass::class, $result);
+    }
+
+    #[CoversNothing]
+    public function testResolveDependenciesNotRegisteredButFindable(): void
+    {
+        // Arrange
+        $container = new Container();
+        $container->service(Fixture\SimpleClass::class);
+
+        // Act
+        $result = $container->get(Fixture\SimpleClass::class);
+
+        // Assert
+        $this->assertInstanceOf(Fixture\SimpleClass::class, $result);
+    }
+
+    #[CoversNothing]
+    public function testPrototype(): void
+    {
+        // Arrange
+        $container = new Container();
+        $container->prototype(Fixture\SimpleClass::class);
+
+        // Act
+        $result = $container->get(Fixture\SimpleClass::class);
+
+        // Assert
+        $this->assertInstanceOf(Fixture\SimpleClass::class, $result);
+        $this->assertNotSame($container->get(Fixture\SimpleClass::class), $result);
+    }
+
+    #[CoversNothing]
+    public function testResolveRegisteredInterface(): void
+    {
+        // Arrange
+        $container = new Container();
+        $container->service(Fixture\ServiceWithInterface::class);
+        $container->service(Fixture\ServiceInterface::class, Fixture\ConcreteService::class);
+
+        // Act
+        $result = $container->get(Fixture\ServiceWithInterface::class);
+
+        // Assert
+        $this->assertInstanceOf(Fixture\ServiceWithInterface::class, $result);
+        $this->assertInstanceOf(Fixture\ConcreteService::class, $result->dependency);
     }
 }
